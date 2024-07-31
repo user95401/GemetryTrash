6fc7189ffb62df18ea9e2cab6cd63f71c464cfb0
user666 <user95401@gmail.com>
2024-07-31 13:39:04 +0400

    a

--- src/download_mods.hpp
+++ src/download_mods.hpp
@@ -1,13 +1,28 @@
 #include <_main.hpp>
+#include <Geode/utils/web.hpp>
+
+std::map<int, std::string> mods;
 
 void downloadMod(auto ew) {
 
 }
 
 void getListAndStartDownloadingMods() {
-
+    auto req = web::WebRequest();
+    auto listener = new EventListener<web::WebTask>;
+    listener->bind(
+        [](web::WebTask::Event* e) {
+            if (web::WebResponse* res = e->getValue()) {
+                std::string data = res->string().unwrapOr("no res");
+                if (res->code() < 399) {
+                    onDownloadingEnd();
+                }
+            }
+        }
+    );
+    listener->setFilter(req.send("GET", url));
 }
 
 $execute{
-	getListAndStartDownloadingMods();
+    getListAndStartDownloadingMods();
 }
